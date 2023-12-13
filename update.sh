81363b7a379eda02c8e61980059936fa22bb20f5
figogove <102362899+figogove@users.noreply.github.com>
2023-12-13 14:27:01 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -63,7 +63,7 @@ sleep 2
 rm -rf /tmp/menu
 wget -O /tmp/menu-master.zip "https://raw.githubusercontent.com/figogove/vps/main/config/menu.zip"
     mkdir /tmp/menu
-    7z e  /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
+    7z e  /tmp/menu-master.zip -o/tmp/menu/
     chmod +x /tmp/menu/*
     mv /tmp/menu/* /usr/sbin/
 
